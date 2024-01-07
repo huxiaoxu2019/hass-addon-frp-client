5bd38e64be96e22b3505849e1b39b85958bbbf8f
Xiaoxu Hu <admin@ihuxu.com>
2024-01-08 01:00:19 +0800

    :wrench:

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -8,7 +8,7 @@ function stop_frpc() {
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-bashio::log.warning "Copying configuration."
+bashio::log.info "Copying configuration."
 cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
 sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
 sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
@@ -16,7 +16,7 @@ sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken'
 sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
 sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
 sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
-sed -i "s/customDomains = [\"your_domain\"]/customDomains = [\"$(bashio::config 'proxies.customDomain')\"]/" $CONFIG_PATH
+sed -i "s/customDomains = [\"your_domain\"]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
 
 bashio::log.info "Starting frp client"
 
@@ -24,6 +24,8 @@ cat $CONFIG_PATH
 
 cd /usr/src
 ./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
+
+tail -f /share/frpc.log &
 
 trap "stop_frpc" SIGTERM SIGHUP
 wait "${WAIT_PIDS[@]}"
