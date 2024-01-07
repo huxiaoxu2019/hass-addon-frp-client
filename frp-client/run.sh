e3b154a027d60036ed625c108f4782d42389da70
Xiaoxu Hu <admin@ihuxu.com>
2024-01-07 18:11:51 +0800

    :wrench:

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bashio
 WAIT_PIDS=()
-CONFIG_PATH='/share/frp/frpc.toml'
+CONFIG_PATH='/share/frpc.toml'
+DEFAULT_CONFIG_PATH='/frpc.toml'
 
 function stop_frpc() {
     bashio::log.info "Shutdown frpc client"
@@ -8,7 +9,14 @@ function stop_frpc() {
 }
 
 if [ ! -f $CONFIG_PATH ]; then
-    bashio::log.error "No frpc.toml file"
+    bashio::log.warning "No frpc.toml file found, copying default configuration."
+    cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
+    sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+    sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
+    sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
+    sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
+    sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
+    sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
 fi
 
 bashio::log.info "Starting frp client"
