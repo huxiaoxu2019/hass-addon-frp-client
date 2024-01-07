d960649e137e8b37ae256c52ae5b77ea70020c74
Xiaoxu Hu <admin@ihuxu.com>
2024-01-07 18:16:24 +0800

    :wrench:

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -8,16 +8,14 @@ function stop_frpc() {
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-if [ ! -f $CONFIG_PATH ]; then
-    bashio::log.warning "No frpc.toml file found, copying default configuration."
-    cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-    sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
-    sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
-    sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
-    sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
-    sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
-    sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
-fi
+bashio::log.warning "Copying configuration."
+cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
+sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
+sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
+sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
+sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
+sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
 
 bashio::log.info "Starting frp client"
 
