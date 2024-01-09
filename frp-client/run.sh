02ba670a67f0896bd627d99356529de2f9c3281e
Xiaoxu Hu <admin@ihuxu.com>
2024-01-09 13:00:44 +0800

    Support for Custom Configuration (#5)

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bashio
 WAIT_PIDS=()
-CONFIG_PATH='/share/frp/frpc.toml'
+CONFIG_PATH='/share/frpc.toml'
+DEFAULT_CONFIG_PATH='/frpc.toml'
 
 function stop_frpc() {
     bashio::log.info "Shutdown frpc client"
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-if [ ! -f $CONFIG_PATH ]; then
-    bashio::log.error "No frpc.toml file"
-fi
+bashio::log.info "Copying configuration."
+cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
+sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
+sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
+sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
+sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
+sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
+sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
 
 bashio::log.info "Starting frp client"
 
@@ -17,6 +24,8 @@ cat $CONFIG_PATH
 
 cd /usr/src
 ./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
+
+tail -f /share/frpc.log &
 
 trap "stop_frpc" SIGTERM SIGHUP
 wait "${WAIT_PIDS[@]}"
