c1cf61723a3b5884bbed437ce759ed29362c2f68
luckywaa <luckywaa@gmail.com>
2024-02-20 19:42:21 +0800

    Customize the Proxies' Name Section to Enable Multiple Connection to a Single FRP Server (#9)

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -17,6 +17,8 @@ sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort
 sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
 sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
 sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
+sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
+
 
 bashio::log.info "Starting frp client"
 
